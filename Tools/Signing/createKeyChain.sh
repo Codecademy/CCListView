5aed8e505bfe63b05d6617618958b1659fcc7396
Ian G <yo.ian.g@gmail.com>
2014-07-17 18:31:46 -0400

    trying again

--- Tools/Signing/createKeyChain.sh
+++ Tools/Signing/createKeyChain.sh
@@ -2,7 +2,7 @@
 security create-keychain -p travis ios-build.keychain
 security import ./Tools/Signing/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./Tools/Signing/Distribution\ Certificate.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./Tools/Signing/Distribution\ Certificate.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./Tools/Signing/Distribution\ Certificate.p12 -k ~/Library/Keychains/ios-build.keychain -P "$KEY_PASSWORD" -T /usr/bin/codesign
 security default-keychain -s ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp "./Tools/Signing/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/

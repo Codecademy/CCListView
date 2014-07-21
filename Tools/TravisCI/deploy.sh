cbf3a0f65980483c7b7e73793ea9f9709de3fc4b
Ian G <yo.ian.g@gmail.com>
2014-07-21 14:19:26 -0400

    test against TestFlight deploy first

---
+++ Tools/TravisCI/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Testing on a branch other than master. No deployment will be done."
+  exit 0
+fi
+
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+OUTPUTDIR="$PWD/build/Release-iphoneos"
+
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE" # TODO: swap out for actual build number?
+
+curl http://testflightapp.com/api/builds.json \
+  -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+  -F api_token="$TESTFLIGHT_API_TOKEN" \
+  -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+  -F distribution_lists='Mobile Devs' \
+  -F notes="$RELEASE_NOTES"
